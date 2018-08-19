cf4d14f56469bcfd2ccdb2bcb5fdb7b66f65cd93
owlvano <owlvano@gmail.com>
2018-08-19 17:51:25 +0300

    controller reset to check fetch

--- controllers/main.py
+++ controllers/main.py
@@ -8,7 +8,5 @@ class DoyarushkaClick2dialController(http.Controller):
         '/doyarushka_click2dial/get_my_channel',
         type='json', auth='public')
     def get_my_channel(self, **kw):
-        # my_channel = http.request.env['asterisk.server'].get_my_channel()
-        # return my_channel
-        true_val = http.request.env['asterisk.server'].get_my_channel_true()
-        return true_val
+        my_channel = http.request.env['asterisk.server'].get_my_channel()
+        return my_channel
